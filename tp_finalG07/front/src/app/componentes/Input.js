b96cb6deee9fdab4a88d621232fbc7e5aa1fda6f
tariasjuarez <tarias@pioix.edu.ar>
2025-10-21 10:07:25 -0300

    gfuy

--- tp_finalG07/front/src/app/componentes/Input.js
+++ tp_finalG07/front/src/app/componentes/Input.js
@@ -1,4 +1,4 @@
 'use client'
 
 export default function Input(props) { 
-    return <input type="text" value={props.value} onChange={props.onChange} />}
+    return <input type="text" value={props.value} onChange={props.onChange} placeholder={props.placeholder} />}
--- tp_finalG07/front/src/app/lobby/page.js
+++ tp_finalG07/front/src/app/lobby/page.js
@@ -6,18 +6,40 @@ import styles from './page.module.css'
 import Boton from '../componentes/Boton';
 import Title from '../componentes/Title';
 import { useSocket } from '../hooks/useSocket';
-
+import Input from '../componentes/Input';
+import { useRouter } from 'next/navigation';
 
 
 export default function Lobby() {
+    const router = useRouter();
+    const [nombre, setNombre] = useState("");
+    const [sala, setSala] = useState("");
+    const [contraseña, setContraseña] = useState("");
+
+    function iniciar() {
+        if (nombre === "" || sala === "") {
+        router.push(`./chat?nombre=${nombre}&sala=${sala}&contraseña=${contraseña}`)
+    }
+    function nombrarUsuario(event) {
+        setNombre(event.target.value)
+    }
+    function nombrarSala(event) {
+        setSala(event.target.value)
+    }
+    function cambiarContraseña(event) {
+        setContraseña(event.target.value)
+    }
     return <>
         <div className={styles.body}>
-                <div className={styles.modalContent}>
-                    <Title className={styles.titleLobby} text={"¿Qué queres hacer?"}></Title>
-                    <Boton className={styles.butonLobby} text={"Ingresar a una sala"}></Boton>
-                    <Boton className={styles.butonLobby} text={"Crear una sala"}></Boton>
-                </div>
-           
+            <div className={styles.modalContent}>
+                <Title className={styles.titleLobby} text={"¿Qué queres hacer?"}></Title>
+                <ul>
+                    <li><Input className={styles.butonLobby} placeholder={"Nombre de Sala"} onChange={nombrarUsuario}></Input></li>
+                    <li><Input className={styles.butonLobby} placeholder={"Número de Sala"} onChange={nombrarSala}></Input></li>
+                    <li><Input className={styles.butonLobby} placeholder={"Contraseña"} onChange={cambiarContraseña}></Input></li>
+                </ul>
+                <Boton className={styles.butonLobby} text={"Jugar"} onClick={iniciar}></Boton>
+            </div>
         </div>
     </>
 
