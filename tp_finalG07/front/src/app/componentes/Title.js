d71d9c6674858d66753bb334eb8f8e2342f593f3
victoriaPalacio <vpalacio@pioix.edu.ar>
2025-11-12 10:13:33 -0300

    aesteti

--- tp_finalG07/front/src/app/componentes/Title.js
+++ tp_finalG07/front/src/app/componentes/Title.js
@@ -2,8 +2,8 @@
 
 export default function Title(props) {
     return (
-        <h1 className={props.className}>
+        <h2 className={props.className}>
             {props.text}
-        </h1>
+        </h2>
     );
 }
