12fa4cab246150926b65d13dc5a2bfe9f659c1c0
simonAnesini <sanesini@pioix.edu.ar>
2025-11-12 11:40:50 -0300

    arreglando quilombitos

--- tp_finalG07/front/src/app/salaEspera/page.js
+++ tp_finalG07/front/src/app/salaEspera/page.js
@@ -6,6 +6,7 @@ import Title from "../componentes/Title";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { agregarASala, buscarEnSala, definirImpostor, palabraAleatoria } from "../fetch/fetch";
+import Boton from "../componentes/Boton";
 
 let siempre = true;
 
@@ -16,6 +17,7 @@ export default function salaEspera() {
   const [jugadores, setJugadores] = useState([]);
   const [idImpostor, setIdImpostor] = useState([]);
   const [palabrita, setPalabrita] = useState("");
+  const [comenzar, setComenzar] = useState(false);
   const router = useRouter();
   const searchParams = useSearchParams();
   const nombre = searchParams.get("nombre");
@@ -23,7 +25,7 @@ export default function salaEspera() {
   const id = searchParams.get("id");
   const admin = searchParams.get("admin")
   const usuario = searchParams.get("usuario")
-  
+
 
 
 
@@ -34,7 +36,45 @@ export default function salaEspera() {
       await agregarASala({ idUser: id, idRoom: sala, esAdmin: admin });
     }
     armadorDeSalas()
-    const intervalo = setInterval(() => {
+   
+
+  }, []);
+  useEffect(() => {
+    if (comenzar) {
+      if (segundos === 1) {
+        if (admin) {
+          async function buscaSalas() {
+            const lista = await buscarEnSala({ idRoom: sala });
+            setJugadores(lista);
+
+            if (lista.length > 0) {
+              const randomIndex = Math.floor(Math.random() * lista.length);
+              const impostorinador = lista[randomIndex];
+              const respuesta = await definirImpostor({
+                idUser: impostorinador.idUser,
+                idRoom: sala
+              });
+              const res = await palabraAleatoria();
+              console.log("Respuesta definir impostor: ", respuesta);
+              console.log("Respuesta palabra aleatoria: ", res);
+              setIdImpostor(respuesta.impostor);
+              setPalabrita(res[0].palabra);
+              console.log("Palabra asignada: ", res[0].palabra);
+            }
+          }
+          buscaSalas();
+        }
+      }
+
+      if (segundos === 3) {
+        router.push(`./chat?usuario=${usuario}&nombre=${nombre}&sala=${sala}&id=${id}&admin=${admin}&impostor=${idImpostor}&encriptaciónSecretaEdgy=asdpfioewvuoqgfu05v8uq34fvu2340568tu2n0guj6f293umn06t5ijt9384kuy3409kb3lvbu6834908tvuwe309gv82b&palabra=${palabrita}`);
+      }
+    }
+  }, [comenzar, segundos, palabrita]);
+
+  function iniciar() {
+    setComenzar(true)
+     const intervalo = setInterval(() => {
       setSegundos((prevSegundos) => prevSegundos + 1);
     }, 1000);
     setIdIntervalo(intervalo);
@@ -42,38 +82,8 @@ export default function salaEspera() {
     return () => {
       clearInterval(intervalo);
     };
-
-  }, []);
-  useEffect(() => {
-  if (segundos === 7) {
-    if (admin) {
-      async function buscaSalas() {
-        const lista = await buscarEnSala({ idRoom: sala });
-        setJugadores(lista);
-
-        if (lista.length > 0) {
-          const randomIndex = Math.floor(Math.random() * lista.length);
-          const impostorinador = lista[randomIndex];
-          const respuesta = await definirImpostor({
-            idUser: impostorinador.idUser,
-            idRoom: sala
-          });
-          const res = await palabraAleatoria();
-          console.log("Respuesta definir impostor: ", respuesta);
-          console.log("Respuesta palabra aleatoria: ", res);
-          setIdImpostor(respuesta.impostor);
-          setPalabrita(res[0].palabra);
-          console.log("Palabra asignada: ", res[0].palabra);
-        }
-      }
-      buscaSalas();
-    }
   }
 
-  if (segundos === 10) {
-    router.push(`./chat?usuario=${usuario}&nombre=${nombre}&sala=${sala}&id=${id}&admin=${admin}&impostor=${idImpostor}&encriptaciónSecretaEdgy=asdpfioewvuoqgfu05v8uq34fvu2340568tu2n0guj6f293umn06t5ijt9384kuy3409kb3lvbu6834908tvuwe309gv82b&palabra=${palabrita}`);
-  }
-}, [segundos,palabrita]);
 
   return (
     <>
@@ -84,6 +94,7 @@ export default function salaEspera() {
             text={"Esperando jugadores..."}
           ></Title>
           <h2 className={styles.temporizador}>Temporizador: {segundos}s</h2>
+          <Boton onClick={iniciar} text="iniciar partida"> </Boton>
         </div>
       </div>
     </>
